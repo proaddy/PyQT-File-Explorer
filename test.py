48ea0d0ef6a71dae485f61170943214572edee55
AdarshVishwakarma <38201144+proaddy@users.noreply.github.com>
2025-01-23 22:28:05 +0530

    commented

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
+
+# Importing the components
 from PySide6.QtWidgets import QApplication, QWidget
+
+# Module responsible for processing cmd arguments
 import sys
 
+# creating application handles: running, click events and etc.
 app = QApplication(sys.argv)
 
+# making widget
 window = QWidget()
+
+# shows widget on the panel
 window.show()
 
+# start the event loop
 app.exec()
