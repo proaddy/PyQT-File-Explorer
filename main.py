5b2ad25a042643d99f1235800eb20c60b6a1a193
AdarshVishwakarma <38201144+proaddy@users.noreply.github.com>
2025-01-23 22:46:13 +0530

    Made into different classes

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 import sys
 
+class ButtonHolder(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Button holder")
+        button = QPushButton("Press me")
+
+        # button as central widget
+        self.setCentralWidget(button)
+
+app = QApplication(sys.argv)
+
+window = ButtonHolder()
+window.show()
+app.exec()
+
+
+
+
+'''
+# contains only global scope 
 app = QApplication(sys.argv)
 
 window = QMainWindow()
@@ -13,3 +33,4 @@ window.setCentralWidget(button)
 
 window.show()
 app.exec()
+'''
